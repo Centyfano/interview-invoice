7c4cc7b0867e9d59f5b2fe6fe0b404ebb38d5fbd
Centyfano <caongare@gmail.com>
2022-08-25 21:24:01 +0300

    table horizontal and vertical lines

--- src/app/create-invoice/gen-pdf/gen-pdf.component.ts
+++ src/app/create-invoice/gen-pdf/gen-pdf.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { Node } from 'pdfmake/interfaces';
 (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
@@ -150,16 +151,29 @@ export class GenPdfComponent implements OnInit {
         },
         {
           layout: {
-            hLineStyle: (i: any, node: any) => {
-              return { dash: { length: 10, space: 0 } };
-              // return { dash: { length: 4 } };
+            hLineWidth: function (i: number, node: Node) {
+              return i === 0 || i === node.table?.body.length ? 1 : 1;
             },
-            vLineStyle: (
-              i: number,
-              node: { table: { widths: string | any[] } }
-            ) => {
-              return { dash: null };
+            vLineWidth: function (i: number, node: Node) {
+              return i === 0 || i === node.table?.widths?.length ? 0 : 0;
             },
+            hLineColor: function (i: number, node: Node) {
+              return i === 0 || i === node.table?.body.length
+                ? 'black'
+                : 'gray';
+            },
+            vLineColor: function (i: number, node: Node) {
+              return i === 0 || i === node.table?.widths?.length
+                ? 'black'
+                : 'gray';
+            },
+            // hLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
+            // vLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
+            // paddingLeft: function(i, node) { return 4; },
+            // paddingRight: function(i, node) { return 4; },
+            // paddingTop: function(i, node) { return 2; },
+            // paddingBottom: function(i, node) { return 2; },
+            // fillColor: function (rowIndex, node, columnIndex) { return null; }
           },
           table: {
             // headerRows: 1,
